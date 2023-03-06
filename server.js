0b3f994cda96bdabb73bcca2daee37de0b7ce4d6
avorio <avorio@mail.ru>
2023-03-05 22:01:13 -0500

    Request counter

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const Queue = require("./queue");
+const cron = require("node-cron");
 
 const interval = JSON.parse(process.env.INTERVAL);
 
 const app = express();
 const queue = new Queue(interval);
 
+let requestCount = 0;
+
 app.use(express.json());
 
 app.post("/tasks", async (req, res) => {
+  requestCount++;
   const { task } = req.body;
   await queue.push(task);
   res.sendStatus(200);
@@ -16,6 +20,12 @@ app.post("/tasks", async (req, res) => {
 
 app.get("/healthcheck", async (req, res) => {
   res.sendStatus(200);
+});
+
+// Reset request counter and log every minute
+cron.schedule("* * * * *", () => {
+  console.log(`Requests per minute: ${requestCount}`);
+  requestCount = 0;
 });
 
 app.listen(process.env.PORT, () => {
